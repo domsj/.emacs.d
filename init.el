6a8a8bff1849cecdcd51f8faeb8a9a269c70861c
domsj <jan.doms@gmail.com>
2013-01-02 22:44:47 +0100

    contract-region

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-+") 'er/contract-region)
 
 ;(require 'unbound)
 (load-file "~/.emacs.d/unbound.el")
