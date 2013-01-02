9c6cac6b312bc6123c7f41d97414073c98ecf0d5
domsj <jan.doms@gmail.com>
2013-01-02 22:03:42 +0100

    addex expand region and .gitignore

--- init.el
+++ init.el
@@ -3,6 +3,15 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+(add-to-list 'load-path
+  (expand-file-name "expand-region.el" user-emacs-directory))
+
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;(require 'unbound)
+(load-file "~/.emacs.d/unbound.el")
+
 ; wind move http://emacswiki.org/emacs/WindMove
 (when (fboundp 'windmove-default-keybindings)
       (windmove-default-keybindings))
