7fcd591add1674bd226d560c0ee14c1eb8e06f4a
domsj <jan.doms@gmail.com>
2013-01-04 23:44:18 +0100

    fancier goto-line

--- init.el
+++ init.el
@@ -65,6 +65,18 @@
 (require 'twittering-mode)
 (setq twittering-use-master-password t)
 
+; fancier goto-line from http://whattheemacsd.com/key-bindings.el-01.html
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+
 (put 'upcase-region 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
